1ab449ef2b8bdcb64f9dc9a3de53be12e982e8a1
“dilshad-shibin” <msdcva786@gmail.com>
2021-12-17 17:51:15 +0530

    about-page added

---
+++ about.php
@@ -0,0 +1,318 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <title>Dianova Lab</title>
+    <!-- Style css -->
+    <?php include('header/head.php'); ?>
+</head>
+
+<body class="homepage-1">
+
+    <!--====== Scroll To Top Area Start ======-->
+    <div id="scrollUp" title="Scroll To Top">
+        <i class="fa fa-angle-up"></i>
+    </div>
+
+    <div class="main">
+        <!-- ***** Header Start ***** -->
+        <?php include('header/header.php'); ?>
+
+        <!-- ***** Breadcrumb Area Start ***** -->
+        <section class="breadcrumb-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-left">
+                        <div class="bread-head">
+                            <h1>About Us</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- ***** About Content Area Start ***** -->
+        <section class="section about-content-area ptb_100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <div class="content-area">
+                            <p>Established in 2001 Dianova a unit of Dianova Diagnostics Pvt. Ltd. is fast emerging as a leader in Medical Laboratory services in Kerala.  Having set up its ultramodern Central processing Laboratory in Kottayam,Trivandrum and Eranakulam, Dianova Laboratories is in the process of setting up centers all over the State of Kerala. As a Committed partner in health, we have earned our exceptional reputation by offering unparalleled service tio our clients, care and compassion to our patients, and access to vast medical and technical resources to our customers.</p>
+                            <p>Dianova Laboratories offers a wide range of health checkup packages to cover all age groups and all the sexes. These packages have been very thoughtfully formulated by a team of highly qualified and experienced medical professionals.  All Investigations are carries out on the art computerized equipments. Dianova is Kerala's best fully automated diagnostic laboratary with a focus on providing quality services at affordable costs all over Kerala.</p>
+                            <p>Dianova is Kerala's best ICMR approved COVID-19 testing centre. We have focus on strong technologies, strong brands and strong systems that enable our laborataries to give their clients the best of science and technology at an affordable cost.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ***** About Counting Area Start ***** -->
+        <section class="about-counting">
+            <div class="count-bg-bottom"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="counter-bx">
+                            <div class="head-count">
+                                <h2 class="counter">20000</h2>
+                                <span>+</span>
+                            </div>
+                            <p>Happy Patients</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="counter-bx">
+                            <div class="head-count">
+                                <h2 class="counter">129</h2>
+                                <span>+</span>
+                            </div>
+                            <p>Qualified Doctors</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="counter-bx">
+                            <div class="head-count">
+                                <h2 class="counter">850</h2>
+                                <span>+</span>
+                            </div>
+                            <p>Clinic Rooms</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="counter-bx">
+                            <div class="head-count">
+                                <h2 class="counter">456</h2>
+                                <span>+</span>
+                            </div>
+                            <p>Awards Won</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ***** Board of Directors Area Start ***** -->
+        <section class="directors-area section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="director-head">
+                            <h2><span>Board of</span> Directors</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="director-box">
+                            <div class="drimg">
+                                <img src="assets/img/directors/1.jpg" alt="">
+                            </div>
+                            <div class="dr-details">
+                                <span>Director</span>
+                                <h3>Dr Sajimon Thomas</h3>
+                                <p>MSc.Phd.(Medical Biochemistry)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="director-box">
+                            <div class="drimg">
+                                <img src="assets/img/directors/2.jpg" alt="">
+                            </div>
+                            <div class="dr-details">
+                                <span>Director</span>
+                                <h3>Dr Sudheesh M</h3>
+                                <p>MSc.Phd.(Medical Biochemistry)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="director-box">
+                            <div class="drimg">
+                                <img src="assets/img/directors/3.jpg" alt="">
+                            </div>
+                            <div class="dr-details">
+                                <span>Director</span>
+                                <h3>Dr Jugan J</h3>
+                                <p>MSc.Phd.Dmrt(Barc)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="director-box">
+                            <div class="drimg">
+                                <img src="assets/img/directors/4.jpg" alt="">
+                            </div>
+                            <div class="dr-details">
+                                <span>Director</span>
+                                <h3>Dr Monu S</h3>
+                                <p>MSc.Mlt.Llb</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ***** Our Team Area Start ***** -->
+        <section  class="our-team section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                    <div class="director-head">
+                            <h2><span>Our</span> Team</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="team-details">
+                            <h3>Dr. Sruthi Thomas</h3>
+                            <p>Mbbs.Md</p>
+                            <span>Microbiologist</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="team-details">
+                            <h3>Dr. Christopher Udayan C</h3>
+                            <p>Mbbs.Md</p>
+                            <span>Pathologist</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="team-details pl-28">
+                            <h3>Dr. Surya A Krishnan</h3>
+                            <p>Mbbs.Dcp</p>
+                            <span>Pathologist</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="team-details">
+                            <h3>Dr. Snehal Somwanshi</h3>
+                            <p>Mbbs.Md</p>
+                            <span>Radiologist</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="team-details">
+                            <h3>Dr. Tiju Chacko</h3>
+                            <p>MSc.Phd</p>
+                            <span>Molecular Virologist</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ***** Policy  Area Start ***** -->
+        <section class="policy-area section ptb_100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <div class="policy-head">
+                            <h1>Our Policies</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <div class="policy-text">
+                            <p>We dedicate ourselves towards excellence in healthcare by providing quality diagnostic services following approved test methodologies and using a diligent professional team and strive to achieve satisfaction of its user.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- ***** Quality and Focus Area Start ***** -->
+        <section class="qulaity-area sectio d-flex flex-wrap">
+            <div class="quality-box">
+                <div class="head">
+                    <h1><span>Quality</span> Objectives</h1>
+                </div>
+                <div class="text">
+                    <ul>
+                        <li class="d-flex">
+                            <div class="img-dtb"><img src="assets/img/dot.png" alt=""> </div>
+                            <div>To comply with ISO 15189:2012 standards at all time.</div>
+                        </li>
+                        <li class="d-flex">
+                            <div class="img-dtb"><img src="assets/img/dot.png" alt=""> </div>
+                            <div>To maintain excellence and keeping pace with the latest developments in clinical diagnostic services.</div>
+                        </li>
+                        <li class="d-flex">
+                            <div class="img-dtb"><img src="assets/img/dot.png" alt=""> </div>
+                            <div>To ensure compliance with statutory and regulatory requirements.</div>
+                        </li>
+                        <li class="d-flex">
+                            <div class="img-dtb"><img src="assets/img/dot.png" alt=""> </div>
+                            <div>To ensure adequate competence and resources, enabling effective & reliable diagnostic services.</div>
+                        </li>
+                        <li class="d-flex">
+                            <div class="img-dtb"><img src="assets/img/dot.png" alt=""> </div>
+                            <div>To ensure on time completion of lab tests and prompt delivery of reports.</div>
+                        </li>
+                        <li class="d-flex">
+                            <div class="img-dtb"><img src="assets/img/dot.png" alt=""> </div>
+                            <div>To fulfill the health care needs & quality expectations of our patients and clinical consultants.</div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="focus-box">
+                <div class="head">
+                    <h1><span>Focus</span> On Customer</h1>
+                </div>
+                <div class="text">
+                    <p>We strive to provide every patient and every customer with services and products of uncompromising quality - error free results on time. Every time we do that by dedicating ourselves to the determined quest of excellence in the services we provide.</p>
+                    <p>We persistently seek creative ways to develop patient care and offer value to our customers. We support the creativity, courage and persistence that transform information into knowledge, and knowledge into insights.</p>
+                </div>
+            </div>
+        </section>
+
+        <!-- ***** Contact Banner Area Start ***** -->
+        <section class="contact-banner contact-banner2 section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="ct-box">
+                            <div class="ct-right-bg"></div>
+                            <div class="health-bg-bottom"></div>
+                            <div class="ct-bx-inner">
+                                <div class="head"><h1>Lorem ipsum is simply dummy text</h1></div>
+                                <div class="text"><p>Lorem Ipsum is simply dummy text of the printing and 
+                                    typesetting industry. </p></div>
+                                <div class="ct-bx-btns d-flex align-items-center">
+                                    <div class="bx-one">
+                                        <a href="#" data-toggle="modal" data-target="#exampleModal0">Book Now</a>
+                                    </div>
+                                    <div class="bx-two d-flex align-items-center">
+                                        <div class="icon"><i class="fas fa-phone-alt"></i></div>
+                                        <div class="num d-flex flex-column">
+                                            <a href="tel:04812303722">0481 2303722</a>
+                                            <a href="tel:04812583722" class="num-last">0481 2583722</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--====== Footer Area Start ======-->
+        <?php include('footer/footer.php'); ?>
+
+    </div>  
+
+    <!-- ***** All jQuery Plugins ***** -->
+
+    <?php include('footer/script.php'); ?>
+
+</body>
+
+</html>
