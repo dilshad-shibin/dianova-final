72a7a21c8adebe8b22c07bf6a4f2cee42aa6b08b
“dilshad-shibin” <msdcva786@gmail.com>
2021-12-20 18:03:17 +0530

    single service aded

---
+++ biochemistry.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <title>Dianova Lab</title>
+    <!-- Style css -->
+    <?php include('header/head.php'); ?>
+</head>
+
+<body class="homepage-1">
+
+    <!--====== Scroll To Top Area Start ======-->
+    <div id="scrollUp" title="Scroll To Top">
+        <i class="fa fa-angle-up"></i>
+    </div>
+
+    <div class="main">
+        <!-- ***** Header Start ***** -->
+        <?php include('header/header.php'); ?>
+
+        <!-- ***** Breadcrumb Area Start ***** -->
+        <section class="breadcrumb-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-left">
+                        <div class="bread-head">
+                            <h1>Biochemistry</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- ***** Single Service Area Start ***** -->
+        <section class="single-service section ptb_100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="service-sidebar">
+                            <h3>Other Services</h3>
+                            <ul>
+                                <li><a href="cytopathology.php"><i class="fas fa-long-arrow-alt-right"></i> Cytopathology</a></li>
+                                <li><a href="doppler-ultrasound.php"><i class="fas fa-long-arrow-alt-right"></i> Doppler Ultrasound</a></li>
+                                <li><a href="serology.php"><i class="fas fa-long-arrow-alt-right"></i> Serology</a></li>
+                                <li><a href="radiology.php"><i class="fas fa-long-arrow-alt-right"></i> Radiology</a></li>
+                                <li><a href="prenatal-screening.php"><i class="fas fa-long-arrow-alt-right"></i> Prenatal Screening</a></li>
+                                <li><a href="microbiology.php"><i class="fas fa-long-arrow-alt-right"></i> Microbiology</a></li>
+                                <li><a href="endocrinology.php"><i class="fas fa-long-arrow-alt-right"></i> Endocrinology</a></li>
+                                <li><a href="allergy-profiles.php"><i class="fas fa-long-arrow-alt-right"></i> Allergy Profiles</a></li>
+                                <li><a href="imaging-ultrasound-scanning.php"><i class="fas fa-long-arrow-alt-right"></i> Imaging(Advanced 4D Ultrasound Scanning)</a></li>
+                                <li><a href="histopathology.php"><i class="fas fa-long-arrow-alt-right"></i> Histopathology</a></li>
+                                <li><a href="clinical-pathology.php"><i class="fas fa-long-arrow-alt-right"></i> Clinical Pathology</a></li>
+                                <li><a href="hematology.php"><i class="fas fa-long-arrow-alt-right"></i> Hematology</a></li>
+                                <li><a href="clinical-chemistry.php"><i class="fas fa-long-arrow-alt-right"></i> Clinical Chemistry</a></li>
+                                <li><a href="immunoassay.php"><i class="fas fa-long-arrow-alt-right"></i> Immunoassay</a></li>
+                                <li><a href="molecular-biology.php"><i class="fas fa-long-arrow-alt-right"></i> Molecular Biology</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="service-wrapper">
+                            <div class="service-thumbnail">
+                                <img src="assets/img/services/biochemistry-700-400.jpg" alt="">
+                            </div>
+                            <h2>Biochemistry</h2>
+                            <div class="text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut doloribus omnis quas error ad, rerum dolorem quisquam a voluptate placeat. Laborum impedit facere nam commodi amet nihil delectus debitis similique harum odio accusamus quod assumenda eos sapiente repellat quasi, unde libero officia, temporibus excepturi minima. Assumenda porro ullam nostrum modi.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem culpa repellendus neque, nobis laborum, cumque at aliquam ipsa vitae explicabo commodi mollitia? Reiciendis, ipsum dolor.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--====== Footer Area Start ======-->
+        <?php include('footer/footer.php'); ?>
+
+    </div>  
+
+    <!-- ***** All jQuery Plugins ***** -->
+
+    <?php include('footer/script.php'); ?>
+
+</body>
+
+</html>
