3f5b711d11a72576bbc613f7d97cb615b51ee681
“dilshad-shibin” <msdcva786@gmail.com>
2021-12-27 15:36:06 +0530

    favicon added

--- header/head.php
+++ header/head.php
@@ -3,7 +3,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Dianova Lab</title>
-<link rel="icon" href="assets/img/favicon.png">
+<link rel="icon" type="image/x-icon" sizes="16x16" href="assets/img/logo/favicon.ico">
 <!-- Style css -->
 <link rel="stylesheet" href="assets/css/style.css">
 <!-- Responsive css -->
